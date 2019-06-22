070a257720224184fbf6e82a57d77457955ded42
Alam <alamhasabie165@gmail.com>
2019-06-22 09:02:12 +0700

    Added django back auth

--- skippedia/skippedia/settings.py
+++ skippedia/skippedia/settings.py
@@ -103,9 +103,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTHENTICATION_BACKENDS = (
- 'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
- 'social_core.backends.google.GoogleOpenId',  # for Google authentication
- 'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+    'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+    'social_core.backends.google.GoogleOpenId',  # for Google authentication
+    'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+    'django.contrib.auth.backends.ModelBackend',
 )
 
 
